fe73b615f0b2360a612028efc51bd7a85ae54a51
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-02-02 12:34:15 -0300

    feat: created theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    darkInk: "#FFFFFF",
+    darkPrimary: "#646464",
+    darkSecondary: "#3F3F3F",
+
+    gray100: "#F2F2F2",
+    gray200: "#D9D9D9",
+    gray300: "#808080",
+    gray400: "#333333",
+    gray500: "#262626",
+    gray600: "#1A1A1A",
+    gray700: "#0D0D0D",
+
+    purple: "#8284FA",
+    purpleDark: "#5E60CE",
+
+    blue: "#4EA8DE",
+    blueDark: "#1E6F9F",
+
+    danger: "#E25858",
+  },
+};
