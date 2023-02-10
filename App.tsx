6012365760728372ba287fc661baf1d04180fc02
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-02-09 21:56:56 -0300

    feat: imported inter font and loading component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { Home } from "./src/screens/Home";
+import { StatusBar } from 'expo-status-bar'
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_700Bold,
+} from '@expo-google-fonts/inter'
+import { Home } from './src/screens/Home'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
-  return (
-    <>
-      <StatusBar style="light" />
-      <Home />
-    </>
-  );
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_700Bold,
+	})
+	return (
+		<>
+			<StatusBar style="light" />
+			{fontsLoaded ? <Home /> : <Loading />}
+		</>
+	)
 }
