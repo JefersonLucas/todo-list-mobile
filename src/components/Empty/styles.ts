231270eac8fa2453a64059e55e24bae4b0c15616
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-02-09 21:53:02 -0300

    refactor: updated style

--- src/components/Empty/styles.ts
+++ src/components/Empty/styles.ts
@@ -1,30 +1,30 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../styles/theme";
+import { StyleSheet } from 'react-native'
+import { theme } from '../../styles/theme'
 
 export const styles = StyleSheet.create({
-  emptyContainer: {
-    flex: 1,
-    alignItems: "center",
-    width: "100%",
-    paddingVertical: 48,
-    paddingHorizontal: 20,
-    borderTopColor: theme.colors.gray400,
-    borderEndColor: theme.colors.gray600,
-    borderStartColor: theme.colors.gray600,
-    borderBottomColor: theme.colors.gray600,
-    borderWidth: 1,
-  },
-  clipboard: {
-    marginBottom: 16,
-  },
-  firstText: {
-    fontSize: 14,
-    fontWeight: "bold",
-    color: theme.colors.gray300,
-  },
-  secondText: {
-    fontSize: 14,
-    fontWeight: "400",
-    color: theme.colors.gray300,
-  },
-});
+	emptyContainer: {
+		flex: 1,
+		alignItems: 'center',
+		width: '100%',
+		paddingVertical: 48,
+		paddingHorizontal: 20,
+		borderTopColor: theme.colors.base.gray400,
+		borderEndColor: theme.colors.base.gray600,
+		borderStartColor: theme.colors.base.gray600,
+		borderBottomColor: theme.colors.base.gray600,
+		borderWidth: 1,
+	},
+	clipboard: {
+		marginBottom: 16,
+	},
+	firstText: {
+		fontSize: theme.font_size.md,
+		fontFamily: theme.font_family.bold,
+		color: theme.colors.base.gray300,
+	},
+	secondText: {
+		fontSize: theme.font_size.md,
+		fontFamily: theme.font_family.regular,
+		color: theme.colors.base.gray300,
+	},
+})
