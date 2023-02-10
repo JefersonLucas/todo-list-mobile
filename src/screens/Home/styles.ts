60d6b3b8ad9e07188987aafe36c636f8afc34806
Jeferson Lucas <jeferson.luckas@gmail.com>
2023-02-09 21:55:25 -0300

    refactor: updated style

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,58 +1,58 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../styles/theme";
+import { StyleSheet } from 'react-native'
+import { theme } from '../../styles/theme'
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: theme.colors.gray600,
-  },
-  tasksContainer: {
-    flex: 1,
-    marginTop: 55,
-    marginHorizontal: 24,
-  },
-  info: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
-  },
-  left: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  right: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  created: {
-    color: theme.colors.blue,
-    fontSize: 14,
-    fontWeight: "bold",
-    marginLeft: 4,
-  },
-  done: {
-    color: theme.colors.purple,
-    fontSize: 14,
-    fontWeight: "bold",
-    marginLeft: 4,
-  },
-  counterContainer: {
-    backgroundColor: theme.colors.gray400,
-    width: 25,
-    height: 19,
-    borderRadius: 999,
-    alignItems: "center",
-    justifyContent: "center",
-    marginLeft: 8,
-  },
-  counterText: {
-    color: theme.colors.gray200,
-    fontSize: 12,
-    fontWeight: "bold",
-  },
-  list: {
-    marginTop: 20,
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: theme.colors.base.gray600,
+	},
+	tasksContainer: {
+		flex: 1,
+		marginTop: 55,
+		marginHorizontal: 24,
+	},
+	info: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		marginBottom: 20,
+	},
+	left: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	right: {
+		flexDirection: 'row',
+		alignItems: 'center',
+	},
+	created: {
+		color: theme.colors.brand.blue,
+		fontSize: theme.font_size.md,
+		fontFamily: theme.font_family.bold,
+		marginLeft: 4,
+	},
+	done: {
+		color: theme.colors.brand.purple,
+		fontSize: theme.font_size.md,
+		fontFamily: theme.font_family.bold,
+		marginLeft: 4,
+	},
+	counterContainer: {
+		backgroundColor: theme.colors.base.gray400,
+		width: 25,
+		height: 19,
+		borderRadius: 999,
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginLeft: 8,
+	},
+	counterText: {
+		color: theme.colors.base.gray200,
+		fontSize: theme.font_size.sm,
+		fontFamily: theme.font_family.bold,
+	},
+	list: {
+		marginTop: 20,
+	},
+})
